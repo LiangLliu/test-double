8dfea32b59ee29b9b12d3fc4a6a8d8ba70af3966
Liangliang Liu <max1141847753@gmail.com>
2020-08-04 21:52:17 +0800

    [Liangliang Liu] 记录笔记

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -10,6 +10,14 @@ public class SecurityCenterTest {
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
 
+    /*
+
+     - 我们不想真的去关门，因此我们可以吧关门的行为mock掉
+     - 在 switchUp(0 方法执行完成后，doorPanel的Mock对象有了交互信息，我们能够去验证 door是否真的被调用
+        我们也不用关注门是否被关闭，因为门关闭是由doorPanel这个类来决定的
+
+     */
+
     DoorPanel doorPanelMock;
 
     @BeforeEach
